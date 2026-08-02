96bcc6adb26582cbcfec765fddc8ec7c79e022c5
pruebas <dariohcn@gmail.com>
2026-08-02 15:49:26 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,8 +4,9 @@
 window.SUPABASE_CONFIG = Object.freeze({
   url: "https://balvytaccwngchtyvllh.supabase.co",
   publishableKey: "sb_publishable_2e0Pz4lUSRFR-SnABW736w_dvlDNbcn",
-  syncIntervalMs: 3000,
-  fullSnapshotIntervalMs: 15000,
+  syncIntervalMs: 1000,
+  eventDebounceMs: 120,
+  fullSnapshotIntervalMs: 10000,
   maxEventsPerSync: 5000,
   requestTimeoutMs: 30000
 });
